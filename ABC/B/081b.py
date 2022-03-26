2a31085f241ad41acbb7ddbf3891c737184203f3
fairy26 <yousei110226@gmail.com>
2022-03-27 07:30:16 +0900

    Add 081B

---
+++ ABC/B/081b.py
@@ -0,0 +1,17 @@
+N = int(input())
+As = list(map(int, input().split()))
+
+count = 0
+finish = False
+while True:
+    for i in range(N):
+        if As[i] % 2 == 0:
+            As[i] = As[i] // 2
+        else:
+            finish = True
+    if finish:
+        break
+    else:
+        count += 1
+
+print(count)
