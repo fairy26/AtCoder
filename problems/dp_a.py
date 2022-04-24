a95765fa791216da4063f43570e291d0f88a5277
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-04-24 23:52:42 +0900

    Add dp_a.py

---
+++ problems/dp_a.py
@@ -0,0 +1,13 @@
+inf = 10 ** 9 + 7
+N = int(input())
+h = list(map(int, input().split()))
+
+dp = [inf] * N
+dp[0] = 0
+
+for i in range(N - 1):
+    dp[i + 1] = min(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]))
+    if i < N - 2:
+        dp[i + 2] = min(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]))
+
+print(dp[-1])
