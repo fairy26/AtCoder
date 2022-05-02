85db8ebbfdd8ca8d6a164286957b444b2fdd02d7
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-05-02 15:28:09 +0900

    Add another solution

--- problems/abc085_b.py
+++ problems/abc085_b.py
@@ -1,20 +1,26 @@
-# ABC085B - Kagami Mochi
+#  ABC085B - Kagami Mochi
+#  解法1
+# N = int(input())
+
+# mochis = []
+# X = 0
+# for i in range(N):
+#     mochis.append(int(input()))
+
+# mochis.sort()
+
+# ex = 0
+# i = N - 1
+# while i >= 0:
+#     if ex != mochis[i]:
+#         ex = mochis[i]
+#     else:
+#         mochis.pop(i)
+#     i -= 1
+
+# print(len(mochis))
+
+#  解法2 - 2022/05/02
 N = int(input())
-
-mochis = []
-X = 0
-for i in range(N):
-    mochis.append(int(input()))
-
-mochis.sort()
-
-ex = 0
-i = N - 1
-while i >= 0:
-    if ex != mochis[i]:
-        ex = mochis[i]
-    else:
-        mochis.pop(i)
-    i -= 1
-
-print(len(mochis))
+d = [int(input()) for _ in range(N)]
+print(len(set(d)))
