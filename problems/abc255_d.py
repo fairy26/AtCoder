9d7719e389a4baedf68bb5074191e5e12b722d1f
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-06-12 04:18:15 +0900

    Update solution

--- problems/abc255_d.py
+++ problems/abc255_d.py
@@ -12,10 +12,22 @@
 #     print(ans)
 
 #  解法2
-N, Q = map(int, input().split())
+import bisect
+
+N, Q = map(int, input().split())  # [1, 2x10^5]
 A = tuple(map(int, input().split()))
 
-for i in range(Q):  # O(2x10^5)
+A = sorted(A)
+sum_A = A[:]  # 累積和
+for i in range(1, N):  # O(N)
+    sum_A[i] += sum_A[i - 1]
+
+for i in range(Q):  # O(N)
     X = int(input())
-    ans = 0
+    k = bisect.bisect_left(A, X)  # O(logN)
+    idx = k - 1
+    if idx >= 0:
+        ans = k * X - sum_A[idx] + (sum_A[-1] - sum_A[idx]) - (N - k) * X  # O(1)
+    else:
+        ans = sum_A[-1] - N * X  # O(1)
     print(ans)
